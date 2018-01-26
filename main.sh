e3f2e065c51000e07ab495cd201525f4880e5168
Jelle Voet <j.voet@wimionline.be>
2018-01-26 17:35:40 +0100

    Partial initial version, figurring out release upgrade

---
+++ main.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+if [[ $(id -u) -ne 0 ]]; then
+  echo "WSL main.sh must be run as root"
+  exit
+fi
+
+function add_config_line {
+  local ACL_FILE="$1"
+  local ACL_LINE="$2"
+  local ACL_REGEX="$(echo "$ACL_LINE" | sed -E 's/([.*\/])/\\\1/g')"
+
+  if [[ -f "$ACL_FILE" ]]; then
+    sed -i "/$ACL_REGEX/d" "$ACL_FILE"
+  fi
+  echo "$ACL_LINE" >> "$ACL_FILE"
+}
+
+# Release upgrade
+# apt-mark hold procps strace sudo bash
+add_config_line "/etc/update-manager/release-upgrades" 'Prompt=normal'
+RELEASE_UPGRADER_NO_SCREEN=1 do-release-upgrade
+
+# Locale fix
+gunzip --keep /usr/share/i18n/charmaps/UTF-8.gz
+dpkg-reconfigure --frontend=noninteractive locales
+
+# Configure repo availability for git(-lfs)
+add-apt-repository ppa:git-core/ppa -y
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+
+# Install packages
+apt-get update
+apt-get install -y \
+  putty-tools \
+  aptitude \
+  git-core \
+  git-lfs \
+  zsh \
+  git
+
+# Maintenance of existing packages
+apt-get upgrade -y
+apt-get autoremove -y
+
+# Install or update antigen
+if [[ ! -d $HOME/.antigen ]]; then
+  git clone https://github.com/zsh-users/antigen.git $HOME/.antigen
+else
+  git -C $HOME/.antigen pull
+fi
+
+# Change config-files
+add_config_line "$HOME/.profile" 'if test -t 1; then exec zsh; fi'
+add_config_line "$HOME/.zshrc" 'source "$HOME/.config/antigen.zsh"'
+add_config_line "$HOME/.zshrc" 'source "$HOME/.config/aliasloading.zsh"'
+add_config_line "$HOME/.zshrc" 'source "$HOME/.config/sshkeys.zsh"'
+
+# Copy config files
+mkdir -p -v $HOME/.config
+cp -v -R ./shellconfig/* $HOME/.config
+
+chown -R $SUDO_USER:$SUDO_USER "$HOME/.zshrc" "$HOME/.profile" "$HOME/.antigen" "$HOME/.config"
+chmod -R go-w "$HOME/.zshrc" "$HOME/.profile" "$HOME/.antigen" "$HOME/.config"
+
+# Switch to zsh
+zsh
+
+# Update antigen
+antigen update
+antigen cache-gen
---
+++ shellconfig/antigen.zsh
@@ -0,0 +1,21 @@
+# .zshrc Source Basic
+source $HOME/.antigen/antigen.zsh
+
+# Load the oh-my-zsh's library.
+antigen use zimfw/zimfw
+
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundle git
+antigen bundle history
+antigen bundle command-not-found
+
+# Theme
+POWERLEVEL9K_INSTALLATION_PATH=$ANTIGEN_BUNDLES/bhilburn/powerlevel9k
+antigen theme bhilburn/powerlevel9k powerlevel9k
+
+# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-completions src
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Tell antigen that you're done.
+antigen apply
