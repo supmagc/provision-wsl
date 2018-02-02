95675430a64b549446b7ce70981a8cdf6e1d34ea
Jelle Voet <j.voet@wimionline.be>
2018-02-02 15:19:36 +0100

    Reduce PS1 script to feature installation only

--- init.ps1
+++ init.ps1
@@ -1,4 +1,4 @@
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile ~/Ubuntu.zip -UseBasicParsing
-Expand-Archive ~/Ubuntu.zip ~/Ubuntu
-~/Ubuntu/ubuntu.exe
+# Enable WSL
+Enable-WindowsOptionalFeature `
+    -FeatureName Microsoft-Windows-Subsystem-Linux `
+    -Online ;
