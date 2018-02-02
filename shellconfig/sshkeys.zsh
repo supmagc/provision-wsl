979c0c1a7cdc23b246e1720ba278ba0117212771
Jelle Voet <j.voet@wimionline.be>
2018-02-02 10:35:29 +0100

    Proper ssh-key conversion

--- shellconfig/sshkeys.zsh
+++ shellconfig/sshkeys.zsh
@@ -1,8 +1,16 @@
-echo $SSH_KEYS
+source "$HOME/.config/config.user.zsh"
+
+mkdir -p -v $HOME/.ssh
+
 if [[ ! -z "$SSH_KEYS" ]]; then
-  for X in "$SSH_KEYS/*.ppk"; do 
-    echo "$X"
-#    puttygen $X -L > ~/.ssh/$(echo $X | sed 's,./,,' | sed 's/.ppk//g').pub
-#    puttygen $X -O private-openssh -o ~/.ssh/$(echo $X | sed 's,./,,' | sed 's/.ppk//g').pvk
+  for X in $SSH_KEYS/*.ppk; do 
+    FILE=$(basename $X)
+    if [[ $FILE == "default.ppk" ]]; then
+      TARGET="id_rsa"
+    else
+      TARGET="id_rsa_${FILE/%.ppk/}"
+    fi
+    puttygen "$X" -O private-openssh -o "$HOME/.ssh/$TARGET"
+    chmod 0600 "$HOME/.ssh/$TARGET"
   done;
 fi
